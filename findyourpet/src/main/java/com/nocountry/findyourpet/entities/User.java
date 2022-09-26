cabeadd276cf943e8d59193a9317e001115d45b4
Hernan Figueroa <gabriel_hernan_2107@hotmail.com>
2022-09-26 13:11:11 -0300

    User Entity update

--- findyourpet/src/main/java/com/nocountry/findyourpet/entities/User.java
+++ findyourpet/src/main/java/com/nocountry/findyourpet/entities/User.java
@@ -1,4 +1,31 @@
 package com.nocountry.findyourpet.entities;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+import static javax.persistence.TemporalType.TIMESTAMP;
+
+@Entity
+@Data
 public class User {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id_user")
+    private Long id;
+
+    private String email;
+    private String password;
+    private String name;
+    private String lastName;
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Role role;
+    private String phone;
+    private String facebookAccount;
+    private Boolean isDeleted;
+
+    @Temporal(TIMESTAMP)
+    private Date createdOn;
 }
