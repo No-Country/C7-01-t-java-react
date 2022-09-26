43d334e94897fe78d65bae80460be9d63c0cfbdd
Nicolas Romano <nicolasromano2@gmail.com>
2022-09-26 17:54:56 -0300

    Role entity update

--- findyourpet/src/main/java/com/nocountry/findyourpet/entities/Role.java
+++ findyourpet/src/main/java/com/nocountry/findyourpet/entities/Role.java
@@ -1,4 +1,21 @@
 package com.nocountry.findyourpet.entities;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "roles")
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    private String description;
+
+
+
 }
