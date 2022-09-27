25b490eb2e8279b6296a41e2f75451bbbab3dbd0
Nicolas Romano <nicolasromano2@gmail.com>
2022-09-27 19:06:29 -0300

    creation repos

---
+++ findyourpet/src/main/java/com/nocountry/findyourpet/repositories/PetRepo.java
@@ -0,0 +1,11 @@
+package com.nocountry.findyourpet.repositories;
+
+import com.nocountry.findyourpet.entities.Pet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface PetRepo extends JpaRepository<Pet, Long> {
+
+}
