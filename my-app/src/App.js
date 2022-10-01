cc944bf93df1733eadffcd090773b16e5a68648f
Hernan Figueroa <gabriel_hernan_2107@hotmail.com>
2022-10-01 18:51:37 -0300

    test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import { Main } from './components/Main';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header></Header>
       <Main></Main>
+      <Footer></Footer>
     </div>
   );
 }
