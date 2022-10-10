7024b054220971c01f388e20aff69809cf08e768
Nicolas Romano <nicolasromano2@gmail.com>
2022-10-10 11:13:02 -0300

    introduction jwt

--- findyourpet/src/main/java/com/nocountry/findyourpet/controller/UserController.java
+++ findyourpet/src/main/java/com/nocountry/findyourpet/controller/UserController.java
@@ -6,13 +6,9 @@ import com.nocountry.findyourpet.models.response.UserResponse;
 import com.nocountry.findyourpet.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/user")
 public class UserController {
 
     @Autowired
@@ -29,6 +25,10 @@ public class UserController {
             return null;
         }
     }
+
+
+
+
 
 
 }
--- findyourpet/src/main/java/com/nocountry/findyourpet/security/WebSecurity.java
+++ findyourpet/src/main/java/com/nocountry/findyourpet/security/WebSecurity.java
@@ -1,14 +1,18 @@
-package com.nocountry.findyourpet;
+package com.nocountry.findyourpet.security;
 
 import com.nocountry.findyourpet.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static com.nocountry.findyourpet.security.Constants.LOGIN_URL;
 
 @Configuration
 @EnableWebSecurity
@@ -27,7 +31,14 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.authorizeRequests().antMatchers("/**").permitAll().and().csrf().disable();
+        http
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .cors().and()
+                .csrf().disable()
+                .authorizeRequests().antMatchers(HttpMethod.POST, LOGIN_URL, "/register").permitAll()
+                .anyRequest().authenticated().and()
+                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+                .addFilter(new JWTAuthorizationFilter(authenticationManager()));
     }
 
 
